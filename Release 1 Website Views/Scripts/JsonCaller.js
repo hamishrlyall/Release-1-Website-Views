5fa8ebbed577f3ec88191c8b3037c3daa4fccc2a
HAMISH LYALL <101614208@student.swin.edu.au>
2018-09-14 13:32:35 +1000

    javascript shenanigans

---
+++ Release 1 Website Views/Scripts/JsonCaller.js
@@ -0,0 +1,5 @@
+﻿var text = '{ "postcard" : [' +
+    '{ "name" : "Charles Lilley" , "profilephoto" : "Images/profile_photo_u17.png", "born" : "3 January 1901 Tocumwal, NSW", "fate" : "Returned to Australia 1 November, Died 16 June 1982, Heidelberg", "service" : "Field Artillery Brigade 8, Battery 29"}]}';
+
+
+var obj = JSON.parse(text);
